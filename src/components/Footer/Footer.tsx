c342e0fbfbbfa7b41a2ea51624380af088c274c2
Jaime Olmo <jolmo@caravellabs.com>
2023-09-06 14:11:18 -0400

    Simple test baseline for UI testing

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,6 @@ import Typography from "@mui/joy/Typography";
 export function Footer() {
   return (
     <Typography sx={{ p: 4 }}>
-      {" "}
       © 2023 Traquéalo — Hecho en Puerto Rico
     </Typography>
   );
