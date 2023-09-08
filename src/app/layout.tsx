fd5123b9422b9ea8f5c0f4e2f72155e094cbf3d6
Jaime Olmo <jolmo@caravellabs.com>
2023-09-08 12:06:10 -0400

    To enable Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeRegistry } from "@/components/ThemeRegistry/ThemeRegistry";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body>
-        <ThemeRegistry>{children}</ThemeRegistry>
+        <ThemeRegistry>
+          {children}
+          <Analytics />
+        </ThemeRegistry>
       </body>
     </html>
   );
