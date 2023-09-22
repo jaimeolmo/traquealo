f53c12ff5a139ab1b029f7384f27df52dc7508c8
Jaime Olmo <jaimeolmo@outlook.com>
2023-09-22 16:45:03 -0400

    More Prettier setup

--- src/app/api/auth/webhooks/route.ts
+++ src/app/api/auth/webhooks/route.ts
@@ -1,8 +1,8 @@
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
-import UserCosmosClient from '@/utilities/cosmosdb/UserCosmosClient'
 import { User } from '@/models/User'
+import UserCosmosClient from '@/utilities/cosmosdb/UserCosmosClient'
+import { WebhookEvent } from '@clerk/nextjs/server'
+import { headers } from 'next/headers'
+import { Webhook } from 'svix'
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
