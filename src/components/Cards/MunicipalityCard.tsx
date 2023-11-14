9e8c3e846848ef931843c6ffb7e51a725afa1957
Jaime Olmo <jaimeolmo@gmail.com>
2023-11-14 17:11:38 -0400

    Baseline for the municipalities dashboard

---
+++ src/components/Cards/MunicipalityCard.tsx
@@ -0,0 +1,50 @@
+import Box from '@mui/joy/Box'
+import Card from '@mui/joy/Card'
+import CardContent from '@mui/joy/CardContent'
+import CardCover from '@mui/joy/CardCover'
+import Stack from '@mui/joy/Stack'
+import Typography from '@mui/joy/Typography'
+
+type ComponentProps = {
+  municipality: {
+    name: string
+    population: number
+    slug: string
+  }
+}
+
+export default function MunicipalityCard({ municipality }: ComponentProps) {
+  return (
+    <Card
+      variant="outlined"
+      orientation="horizontal"
+      sx={{
+        '&:hover': {
+          boxShadow: 'md',
+          borderColor: 'neutral.outlinedHoverBorder',
+        },
+      }}
+    >
+      <CardCover
+        sx={{
+          background: `url("/images/Puerto_Rico_municipios_${municipality.slug}_map.svg")`,
+          backgroundPosition: 'right',
+          backgroundSize: '70%, auto, contain',
+          backgroundRepeat: 'no-repeat',
+          opacity: 0.75,
+        }}
+      />
+      <CardContent>
+        <Typography level="title-lg" id="card-description">
+          {municipality.name}
+        </Typography>
+        <Stack direction="row" spacing={1}>
+          <Box>Población:</Box>
+          <Box sx={{ fontWeight: 'bold' }}>
+            {municipality.population.toLocaleString()}
+          </Box>
+        </Stack>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ src/components/Municipalities/MunicipalitiesList.tsx
@@ -0,0 +1,51 @@
+'use client'
+import MunicipalityCard from '@/components/Cards/MunicipalityCard'
+import Grid from '@mui/joy/Grid'
+import Input from '@mui/joy/Input'
+import { useState } from 'react'
+
+type Municipality = {
+  id: string
+  name: string
+  population: number
+  slug: string
+}
+
+type ComponentProps = {
+  municipalities: Array<Municipality>
+}
+
+export default function MunicipalitiesList({ municipalities }: ComponentProps) {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleSearchChange = (event: { target: { value: any } }) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const filteredMunicipalities = municipalities.filter((municipality) =>
+    municipality.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  )
+
+  return (
+    <>
+      <Input
+        name="Search"
+        color="neutral"
+        placeholder="Filtrar municipios"
+        variant="outlined"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        size="lg"
+        fullWidth
+        style={{ marginBottom: '20px' }}
+      />
+      <Grid container spacing={2}>
+        {filteredMunicipalities.map((municipality) => (
+          <Grid xs={12} md={6} lg={4} key={municipality.id}>
+            <MunicipalityCard municipality={municipality} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  )
+}
