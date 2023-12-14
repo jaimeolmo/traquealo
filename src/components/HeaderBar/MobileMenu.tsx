1a7a91bfae8c3fa70657eb171514fde5382c5315
Jaime Olmo <jaimeolmo@gmail.com>
2023-12-14 14:23:30 -0400

    Mobile menu fix

--- src/components/HeaderBar/MobileMenu.tsx
+++ src/components/HeaderBar/MobileMenu.tsx
@@ -1,83 +1,110 @@
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
-import DragHandleRoundedIcon from '@mui/icons-material/DragHandleRounded'
+import { ClickAwayListener } from '@mui/base/ClickAwayListener'
+import { Popper } from '@mui/base/Popper'
 import Box from '@mui/joy/Box'
 import Button from '@mui/joy/Button'
-import Dropdown from '@mui/joy/Dropdown'
 import IconButton from '@mui/joy/IconButton'
-import Menu from '@mui/joy/Menu'
-import MenuButton from '@mui/joy/MenuButton'
+import MenuList from '@mui/joy/MenuList'
 import Stack from '@mui/joy/Stack'
+import { styled } from '@mui/joy/styles'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { useCallback, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import { UrlObject } from 'url'
 import { ButtonCreate } from '../ButtonCreate'
+
+const Popup = styled(Popper)({
+  zIndex: 1000,
+})
 
 export default function MobileMenu() {
   const pathname = usePathname()
   const [open, setOpen] = useState(false)
   const buttonRef = useRef<HTMLButtonElement>(null)
 
-  const handleOpenChange = useCallback(
-    (event: React.SyntheticEvent | null, isOpen: boolean) => {
-      setOpen(isOpen)
-    },
-    [],
-  )
-
   const handleClose = () => {
     setOpen(false)
   }
 
   return (
-    <Dropdown open={open} onOpenChange={handleOpenChange}>
-      <MenuButton
-        slots={{ root: IconButton }}
-        slotProps={{ root: { variant: 'soft', color: 'secondary' } }}
-      >
-        {open ? <CloseRoundedIcon /> : <DragHandleRoundedIcon />}
-      </MenuButton>
-
-      <Menu placement="bottom-end" sx={{ px: 1 }}>
-        <Stack spacing={1} alignItems={'flex-end'}>
-          <Stack sx={{ width: '100%' }}>
-            <SignedIn>
-              {pathname !== '/reportar' ? (
-                <Link prefetch={false} href="/reportar" passHref>
-                  <Button variant="soft" fullWidth>
-                    Reportar
-                  </Button>
-                </Link>
-              ) : null}
-            </SignedIn>
-          </Stack>
-          <Stack>
-            {pathname !== '/dashboard' ? <ButtonCreate /> : null}
-            <SignedOut>
-              <Box
-                sx={{
-                  '& a': {
-                    color: 'var(--joy-palette-common-white)',
-                  },
-                }}
-              >
-                <Link
-                  prefetch={false}
-                  href={'/sign-in' as unknown as UrlObject}
-                >
-                  Sign in
-                </Link>
-              </Box>
-            </SignedOut>
-          </Stack>
-          <Stack>
-            <SignedIn>
-              <UserButton afterSignOutUrl="/" />
-            </SignedIn>
-          </Stack>
-        </Stack>
-      </Menu>
-    </Dropdown>
+    <Stack direction={'row-reverse'} alignItems={'center'} spacing={1}>
+      <Stack>
+        <IconButton
+          ref={buttonRef}
+          id="composition-button"
+          aria-controls={'composition-menu'}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          variant="soft"
+          color="secondary"
+          onClick={() => {
+            setOpen((prevOpen) => !prevOpen)
+          }}
+        >
+          {open ? 'x' : '='}
+        </IconButton>
+        <Popup
+          role={undefined}
+          id="composition-menu"
+          open={open}
+          anchorEl={buttonRef.current}
+          disablePortal
+          modifiers={[
+            {
+              name: 'offset',
+              options: {
+                offset: [0, 4],
+              },
+            },
+          ]}
+          sx={{ pr: 2 }}
+        >
+          <ClickAwayListener
+            onClickAway={(event) => {
+              if (event.target !== buttonRef.current) {
+                handleClose()
+              }
+            }}
+          >
+            <MenuList variant="outlined" sx={{ boxShadow: 'md', px: 1 }}>
+              <Stack spacing={1} alignItems={'flex-end'}>
+                <Stack sx={{ width: '100%' }} spacing={1}>
+                  <SignedIn>
+                    {pathname !== '/reportar' ? (
+                      <Link prefetch={false} href="/reportar" passHref>
+                        <Button variant="soft" fullWidth>
+                          Reportar
+                        </Button>
+                      </Link>
+                    ) : null}
+                  </SignedIn>
+                  {pathname !== '/dashboard' ? <ButtonCreate /> : null}
+                </Stack>
+              </Stack>
+            </MenuList>
+          </ClickAwayListener>
+        </Popup>
+      </Stack>
+      <Stack>
+        <SignedOut>
+          <Box
+            sx={{
+              '& a': {
+                color: 'var(--joy-palette-common-white)',
+              },
+            }}
+          >
+            <Link prefetch={false} href={'/sign-in' as unknown as UrlObject}>
+              Sign in
+            </Link>
+          </Box>
+        </SignedOut>
+      </Stack>
+      <Stack>
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+      </Stack>
+    </Stack>
   )
 }
