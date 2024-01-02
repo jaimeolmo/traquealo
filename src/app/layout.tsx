93c45d0d6cb9f8c062eb220267af4addf422291f
Jaime Olmo <jaimeolmo@gmail.com>
2024-01-02 14:14:04 -0400

    OG Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,31 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 import Script from 'next/script'
 import './global.css'
 
+export const metadata = {
+  openGraph: {
+    title: 'Traquealo',
+    description:
+      'Construyendo un Futuro Mejor: Monitoreo y Registro de Desafíos Comunitarios',
+    url: 'https://traquealo.com',
+    siteName: 'Traquealo',
+    images: [
+      {
+        url: 'https://traquealo.com/og.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://traquealo.com/og-alt.png', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'Construyendo un Futuro Mejor',
+      },
+    ],
+    locale: 'es_US',
+    type: 'website',
+  },
+}
+
 export default function RootLayout({
   children,
 }: {
