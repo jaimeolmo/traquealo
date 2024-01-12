18de378aaaaece38c8da8bc37b2d5dcd6423b316
Jaime Olmo <jaimeolmo@gmail.com>
2024-01-12 14:27:20 -0400

    Image lightbox

--- src/components/ReportMultimedia/ImageGroup.tsx
+++ src/components/ReportMultimedia/ImageGroup.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
+'use client'
 import Grid from '@mui/joy/Grid'
-import Link from 'next/link'
-import { Key } from 'react'
-import { UrlObject } from 'url'
+import { useState } from 'react'
+import Lightbox from 'yet-another-react-lightbox'
+import Counter from 'yet-another-react-lightbox/plugins/counter'
+import 'yet-another-react-lightbox/plugins/counter.css'
+import 'yet-another-react-lightbox/styles.css'
 
 type ComponentProps = {
   media: [] | never[]
@@ -10,18 +13,22 @@ type ComponentProps = {
 }
 
 export function ImageGroup({ media, sasToken }: ComponentProps) {
+  const [open, setOpen] = useState(false)
+  const [imgIndex, setImgIndex] = useState(0)
+
+  const mediaWithToken = media.map((i: string) => ({
+    src: `${i.replace('/thumb/', '/lg/')}?${sasToken}`,
+  }))
+
   return (
-    <Grid container spacing={1}>
-      {media === undefined
-        ? null
-        : Array.isArray(media)
-        ? media.map((i: Key | null | undefined) => {
-            return (
-              <Grid key={i}>
-                <Link
-                  href={`${i}?${sasToken}` as unknown as UrlObject}
-                  style={{ textDecoration: 'none', color: 'inherit' }}
-                >
+    <>
+      <Grid container spacing={1}>
+        {media === undefined
+          ? null
+          : Array.isArray(media)
+          ? media.map((i: string | null | undefined, index: number) => {
+              return (
+                <Grid key={i}>
                   <img
                     key={i}
                     src={`${i}?${sasToken}`}
@@ -31,12 +38,24 @@ export function ImageGroup({ media, sasToken }: ComponentProps) {
                       width: '100px',
                       borderRadius: 8,
                     }}
+                    onClick={() => {
+                      setImgIndex(index)
+                      setOpen(true)
+                    }}
                   />
-                </Link>
-              </Grid>
-            )
-          })
-        : null}
-    </Grid>
+                </Grid>
+              )
+            })
+          : null}
+      </Grid>
+      <Lightbox
+        plugins={[Counter]}
+        index={imgIndex}
+        counter={{ container: { style: { top: 'unset', bottom: 0 } } }}
+        open={open}
+        close={() => setOpen(false)}
+        slides={mediaWithToken}
+      />
+    </>
   )
 }
