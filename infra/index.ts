6b8b83759c5a3fb1e8a34d5381c389987b2aebd9
Jaime Olmo <jaimeolmo@gmail.com>
2024-03-06 14:35:02 -0400

    Stage and Prod

--- infra/index.ts
+++ infra/index.ts
@@ -1,23 +1,32 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as resources from "@pulumi/azure-native/resources";
-import * as storage from "@pulumi/azure-native/storage";
+import * as resources from '@pulumi/azure-native/resources'
+import * as storage from '@pulumi/azure-native/storage'
+import * as pulumi from '@pulumi/pulumi'
+
+const config = new pulumi.Config()
+const environment = config.require('environment')
+const location = config.require('location')
 
 // Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup("traquealo-prod-rg");
+const resourceGroup = new resources.ResourceGroup(
+  `traquealo-${environment}-rg`,
+  {
+    location: location,
+  },
+)
 
 // Create an Azure resource (Storage Account)
-const storageAccount = new storage.StorageAccount("traquealoprodsa", {
-    resourceGroupName: resourceGroup.name,
-    sku: {
-        name: storage.SkuName.Standard_LRS,
-    },
-    kind: storage.Kind.StorageV2,
-});
+const storageAccount = new storage.StorageAccount(`traquealo${environment}sa`, {
+  resourceGroupName: resourceGroup.name,
+  sku: {
+    name: storage.SkuName.Standard_LRS,
+  },
+  kind: storage.Kind.StorageV2,
+})
 
 // Export the primary key of the Storage Account
 const storageAccountKeys = storage.listStorageAccountKeysOutput({
-    resourceGroupName: resourceGroup.name,
-    accountName: storageAccount.name
-});
+  resourceGroupName: resourceGroup.name,
+  accountName: storageAccount.name,
+})
 
-export const primaryStorageKey = storageAccountKeys.keys[0].value;
+export const primaryStorageKey = storageAccountKeys.keys[0].value
