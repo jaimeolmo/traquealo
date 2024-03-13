2cece7944dabe2e122f67eff8d4365151111a355
Jaime Olmo <jaimeolmo@gmail.com>
2024-03-13 12:54:45 -0400

    CosmosDb and issues container

--- infra/index.ts
+++ infra/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as documentdb from '@pulumi/azure-native/documentdb'
 import * as resources from '@pulumi/azure-native/resources'
 import * as storage from '@pulumi/azure-native/storage'
 import * as pulumi from '@pulumi/pulumi'
@@ -23,11 +25,11 @@ const storageAccount = new storage.StorageAccount(`traquealo${environment}sa`, {
   kind: storage.Kind.StorageV2,
 })
 
-// **Create the new container**
+// Create the media container
 const mediaContainer = new storage.BlobContainer('media', {
   resourceGroupName: resourceGroup.name,
   accountName: storageAccount.name,
-  publicAccess: storage.PublicAccess.None, // Or other access level if needed
+  publicAccess: storage.PublicAccess.None,
 })
 
 // Export the primary key of the Storage Account
@@ -36,6 +38,87 @@ const storageAccountKeys = storage.listStorageAccountKeysOutput({
   accountName: storageAccount.name,
 })
 
-// CosmosDb
+// Create the Cosmos DB Account
+const cosmosDbAccount = new documentdb.DatabaseAccount('databaseAccount', {
+  accountName: `traquealo-${environment}-cdb`,
+  resourceGroupName: resourceGroup.name,
+  databaseAccountOfferType: documentdb.DatabaseAccountOfferType.Standard,
+  kind: documentdb.DatabaseAccountKind.GlobalDocumentDB,
+  consistencyPolicy: {
+    defaultConsistencyLevel: 'Session',
+  },
+  location: location,
+  locations: [
+    {
+      locationName: resourceGroup.location,
+      failoverPriority: 0,
+    },
+  ],
+  ...(environment === 'prod' ? { enableFreeTier: true } : {}),
+  ...(environment === 'staging'
+    ? { capabilities: [{ name: 'EnableServerless' }] }
+    : {}),
+})
+
+// Cosmos DB Database
+const cosmosdbDatabase = new documentdb.SqlResourceSqlDatabase(
+  'sqlResourceSqlDatabase',
+  {
+    accountName: cosmosDbAccount.name,
+    databaseName: `traquealo-${environment}-db`,
+    resourceGroupName: resourceGroup.name,
+    resource: {
+      id: `traquealo-${environment}-db`,
+    },
+  },
+)
+
+// Cosmos DB Containers
+const issuesContainer = new documentdb.SqlResourceSqlContainer(
+  'sqlResourceSqlContainer',
+  {
+    accountName: cosmosDbAccount.name,
+    containerName: 'issues',
+    resourceGroupName: resourceGroup.name,
+    databaseName: cosmosdbDatabase.name,
+    resource: {
+      id: 'issues',
+      partitionKey: {
+        kind: 'Hash',
+        paths: ['/id'], // Define your partition key path
+      },
+    },
+  },
+)
+
+// Get Cosmos DB Connection Strings
+const cosmosDbConnectionStrings =
+  documentdb.listDatabaseAccountConnectionStringsOutput({
+    accountName: cosmosDbAccount.name,
+    resourceGroupName: resourceGroup.name,
+  })
+
+// Define a type for your connection string structure (if needed)
+interface CosmosDbConnectionDetails {
+  connectionString: string
+}
+
+// Use the output to get your connection string with safety checks
+const primaryCosmosDbConnectionDetails = cosmosDbConnectionStrings.apply(
+  (connectionStrings) => {
+    if (
+      !connectionStrings ||
+      !connectionStrings.connectionStrings ||
+      connectionStrings.connectionStrings.length === 0
+    ) {
+      throw new Error('Cosmos DB connection strings not found.')
+    }
+
+    return connectionStrings.connectionStrings[0] as CosmosDbConnectionDetails
+  },
+)
+
+export const primaryConnectionString =
+  primaryCosmosDbConnectionDetails.connectionString
 
 export const primaryStorageKey = storageAccountKeys.keys[0].value
